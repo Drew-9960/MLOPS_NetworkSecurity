c8b33f2f1c59c413f99ff066684982ccafbb3e3f
Drew-9960 <melendeza895@outlook.com>
2025-04-09 21:39:02 -0400

    model_trainer prep

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -15,8 +15,12 @@ TRAIN_FILE_NAME : str = "train.csv"
 TEST_FILE_NAME : str = "test.csv"
 
 SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+
+SAVED_MODEL_DIR = os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
+
 """
-Data ingestion related constant, starting with DATA_INGETSION VAR NAME
+Data ingestion related constant starting with DATA_INGETSION VAR NAME
 """
 
 DATA_INGESTION_COLLECTION_NAME : str = "NetworkData"
@@ -27,7 +31,7 @@ DATA_INGESTION_INGESTED_DIR : str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO : float = 0.2
 
 """
-Data validation related constant, starting with DATA_VALIDATION VAR NAME
+Data validation related constant starting with DATA_VALIDATION VAR NAME
 """
 DATA_VALIDATION_DIR_NAME : str = "data_validation"
 DATA_VALIDATION_VALID_DIR : str = "validated"
@@ -37,7 +41,7 @@ DATA_VALIDATION_DRIFT_REPORT_FILE_NAME : str = "drift_report.yaml"
 PREPROCESSING_OBJECT_FILE_NAME : str = "preprocessing.pkl"
 
 """
-Data transformation related constant start with DATA_TRANSFORMATION VAR NAME
+Data transformation related constant starting with DATA_TRANSFORMATION VAR NAME
 """
 DATA_TRANSFORMATION_DIR_NAME : str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR : str = "transformed"
@@ -51,3 +55,12 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS : dict = {
 }
 DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
 DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
+
+"""
+Model trainer related constant starting with MODEL_TRAINER VAR NAME
+"""
+MODEL_TRAINER_DIR_NAME : str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR : str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME : str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE : float = 0.6
+MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD : float = 0.05
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -23,7 +23,7 @@ def write_yaml_file(file_path : str, content : object, replace : bool = False) -
             with open(file_path, 'w') as file:
                 yaml.dump(content, file)
     except Exception as e:
-        raise NetworkSecurityException(e, sys)
+        raise NetworkSecurityException(e, sys) from e 
     
 def save_numpy_array_data(file_path : str, array : np.array):
     """
@@ -40,7 +40,7 @@ def save_numpy_array_data(file_path : str, array : np.array):
             np.save(file_obj, array)
         
     except Exception as e:
-        raise NetworkSecurityException(e, sys) 
+        raise NetworkSecurityException(e, sys) from e 
     
 def save_object(file_path : str, obj : str) -> None:
     try:
@@ -50,4 +50,21 @@ def save_object(file_path : str, obj : str) -> None:
             pickle.dump(obj, file_obj)
         logging.info('Exited the save_object method of MainUtils class')
     except Exception as e:
-        raise NetworkSecurityException(e, sys) 
+        raise NetworkSecurityException(e, sys) from e  
+
+def load_object(file_path : str, ) -> object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file: {file_path} does not exist")
+        with open(file_path, 'rb') as file_obj:
+            print(file_obj)
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e 
+
+def load_numpy_array(file_path : str) -> np.array:
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e 
