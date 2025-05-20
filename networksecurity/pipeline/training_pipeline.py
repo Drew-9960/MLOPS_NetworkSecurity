c4af10db82d27396863c03df15e860a470c4c768
Drew-9960 <melendeza895@outlook.com>
2025-05-19 21:03:04 -0400

    app

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -27,7 +27,7 @@ from networksecurity.entity.artifact_entity import(
 
 class TrainingPipeline:
     def __init__(self):
-        self.training_pipeline_config = TrainingPipelineConfig
+        self.training_pipeline_config = TrainingPipelineConfig()
         
     def start_data_ingestion(self):
         try:
@@ -37,13 +37,14 @@ class TrainingPipeline:
             data_ingestion = DataIngestion(data_ingestion_config = self.data_ingestion_config)
             data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
             logging.info(f"Data Ingestion completed and artifact : {data_ingestion_artifact}")
+            return data_ingestion_artifact
             
         except Exception as e:
             raise NetworkSecurityException(e,sys)
     
     def start_data_validation(self, data_ingestion_artifact : DataIngestionArtifact):
         try:
-            data_validation_config = DataValidationConfig(trainingpipelineconfig = self.training_pipeline_config)
+            data_validation_config = DataValidationConfig(training_pipeline_config = self.training_pipeline_config)
             data_validation = DataValidation(data_ingestion_artifact = data_ingestion_artifact,data_validation_config = data_validation_config)
             logging.info("Initiate the data Validation")
             data_validation_artifact = data_validation.initiate_data_validation()
@@ -53,7 +54,7 @@ class TrainingPipeline:
         
     def start_data_transformation(self, data_validation_artifact : DataValidationArtifact):
         try:
-            data_transformation_config = DataTransformationConfig(trainingpipelineconfig = self.training_pipeline_config)
+            data_transformation_config = DataTransformationConfig(training_pipeline_config = self.training_pipeline_config)
             logging.info("Data transformation started")
             data_transformation = DataTransformation(data_validation_artifact = data_validation_artifact, data_transformation_config = data_transformation_config)
             data_transformation_artifact = data_transformation.initiate_data_transformation()
